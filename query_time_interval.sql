72bf908e41f8797f8cd533acd0dfcd8fe449f35a
Makshakov Serj <42265508+Serj-M@users.noreply.github.com>
2025-11-27 17:20:41 +0300

    Create query_time_interval.sql

---
+++ query_time_interval.sql
@@ -0,0 +1,50 @@
+-- определяем время затраченное на коммиты, включающие указанные файлы/дирректории, для первого года разработки
+WITH first_commit_year AS (
+    -- Подзапрос для определения года первого коммита, удовлетворяющего условиям
+    SELECT EXTRACT(YEAR FROM MIN(c.commit_created_at)) AS start_year
+    FROM commits c
+    JOIN file_changes fc ON c.id = fc.commit_id  -- Присоединяем информацию об изменяемых файлов в коммите
+    WHERE (fc.file_path LIKE 'app/utils/excel_file_generator%' OR fc.file_path LIKE 'app/utils/simple_table_excel%') AND c.repo_id = 30
+),
+commits_with_first_parent AS (
+    SELECT DISTINCT
+        c.hash AS current_commit_hash,
+        c.repo_id AS commit_repo_id,
+        CASE  -- Используем первый элемент из parent_hashes, если он существует
+            WHEN c.parent_hashes IS NULL OR array_length(c.parent_hashes, 1) = 0 THEN NULL
+            ELSE c.parent_hashes[1]
+        END AS first_parent_hash,
+        c.commit_created_at AS current_commit_time,
+        c.author_name,
+        c.pull_request_id,
+        c.repo_id
+    FROM commits c
+    JOIN file_changes fc ON c.id = fc.commit_id
+    CROSS JOIN first_commit_year fcy   -- Присоединяем найденный первый год
+    WHERE
+        (fc.file_path LIKE 'app/utils/excel_file_generator%' OR fc.file_path LIKE 'app/utils/simple_table_excel%') AND c.repo_id = 30
+        AND EXTRACT(YEAR FROM c.commit_created_at) = fcy.start_year   -- Фильтруем по году
+)
+SELECT
+	  cwp.repo_id,
+    -- Вычисляем интервал как разницу между временем текущего и родительского коммита
+    (cwp.current_commit_time - p.commit_created_at) AS time_interval,
+    cwp.current_commit_time,
+    p.commit_created_at AS parent_commit_time,
+    cwp.author_name,
+    cwp.current_commit_hash,
+    cwp.first_parent_hash,
+    cwp.pull_request_id AS pr_id,
+    pr.title AS pr_title,
+    i.issue_id,
+    i.title AS issue_title,
+    i.url AS issue_url
+FROM commits_with_first_parent cwp
+    -- Присоединяем таблицу commits снова, чтобы получить время родительского коммита
+    JOIN commits p ON p.hash = cwp.first_parent_hash AND p.repo_id = cwp.commit_repo_id
+    -- Присоединяем pull_requests, чтобы попвтаться получить связь с задачами (issues)
+    FULL JOIN pull_requests pr ON cwp.pull_request_id = pr.id
+    -- Присоединяем issues, используя ANY для массива issue_ids
+    FULL JOIN issues i ON i.id = ANY(pr.issue_ids) AND i.repo_id = pr.repo_id
+WHERE cwp.first_parent_hash IS NOT NULL -- Исключаем коммиты без родителя (например, начальный коммит)
+ORDER BY cwp.current_commit_time ASC; -- Сортировка по времени текущего коммита
